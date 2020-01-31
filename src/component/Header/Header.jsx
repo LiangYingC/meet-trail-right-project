08020de3898af7533b37679a89bb86f2add94e97
Liang C <sl20130313@gmail.com>
2020-01-31 11:01:20 +0800

    Complete header component layout

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,17 +1,56 @@
 import React, { Component } from 'react';
-import logo from '../../assets/logo/logo260x70.png';
+import logoImg from '../../assets/logo/logo260x70.png';
+import searchImg from '../../assets/img/search.png';
+import userImg from '../../assets/img/user.png';
+import downArrowImg from '../../assets/img/downArrow.png';
+import './Header.css'
+
 
 class Header extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            isLanguagOptionsOpen: false
+        }
+    }
+
+    toggleLanguagOptions = () => {
+        this.setState((preState) => ({
+            isLanguagOptionsOpen: !preState.isLanguagOptionsOpen
+        }))
+    }
+
     render() {
+        const { isLanguagOptionsOpen } = this.state
         return (
             <section id="header">
                 <div className="wrap">
                     <div className="header-logo">
-                        <img src={logo} alt="選山步道 Logo" />
+                        <img src={logoImg} alt="選山步道 logo" />
                     </div>
                     <div className="header-search">
-                        <input type="text" className="search__input" />
-                        <div className="search__icon"></div>
+                        <input type="text" id="search-input" placeholder="請輸入步道名稱" />
+                        <div className="search-icon">
+                            <img src={searchImg} alt="search logo" />
+                        </div>
+                    </div>
+                    <div className="header-nav">
+                        <ul>
+                            <li>全部步道</li>
+                            <li >
+                                <div id="header-language-btn" onClick={this.toggleLanguagOptions}>
+                                    <p>繁體中文</p>
+                                    <div className={`down-arrow-icon ${isLanguagOptionsOpen ? 'down-arrow-icon--active' : ''}`}><img src={downArrowImg} alt="more options logo" /></div>
+                                    <div className={`language-options-box ${isLanguagOptionsOpen ? 'language-options-box--active' : ''}`} >
+                                        <p>繁體中文</p>
+                                        <p>English</p>
+                                    </div>
+                                </div>
+                            </li>
+                            <div id="header-user-btn">
+                                <img src={userImg} alt="user logo" />
+                            </div>
+                        </ul>
                     </div>
                 </div>
             </section>
