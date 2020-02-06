930018d0d224d7f6a1afa2509458bcd80c55e940
Liang C <sl20130313@gmail.com>
2020-02-06 13:34:35 +0800

    Add congif file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
+import firebaseConfig from './config'
 import React from 'react';
 import ReactDom from 'react-dom';
 import App from './App';
 import './styles/main.scss';
 
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
 
 ReactDom.render(<App />, document.getElementById('root'))
