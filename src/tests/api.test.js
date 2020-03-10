b7fe2126047cb87e90860614a3dadde8ee356004
Liang C <sl20130313@gmail.com>
2020-03-10 14:15:11 +0800

    Complete weather api test

--- src/tests/api.test.js
+++ src/tests/api.test.js
@@ -1,5 +1,15 @@
 import axios from "axios";
-import { youtubeConfig } from '../config';
+import { weatherConfig, youtubeConfig } from '../config';
+
+test("weather API", () => {
+    const weatherUrl = `${weatherConfig.baseUrl}/F-D0047-075?Authorization=${weatherConfig.apiKey}&locationName=大甲區&elementName=MaxAT,MinAT,PoP12h,WeatherDescription`
+    return axios.get(weatherUrl)
+        .then((response) => {
+            const data = response.data
+            const weatherElementList = data.records.locations[0].location[0].weatherElement
+            expect(weatherElementList).toBeDefined()
+        })
+})
 
 test("youtube search API", () => {
     const searchVedioUrl = `${youtubeConfig.baseUrl}/search?part=snippet&type=video
@@ -27,3 +37,4 @@ test("youtube channels API", () => {
             expect(typeof data.items[0].snippet.thumbnails.default.url).toBe('string')
         })
 })
+
